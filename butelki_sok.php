a4e953b33d0da7d1c284ada9fe94cd95a615709e
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-09-24 22:59:31 +0200

    Nowe entery.
    Można uzywać także \r\n jako znaku entera. Jest to "symbol" z linii komend, a <br> jest interpretowane przez przeglądarkę.

--- butelki_sok.php
+++ butelki_sok.php
@@ -1,14 +1,14 @@
 <?php
 $lb = 99; // lb to liczba butelek
 while($lb >= 2){
-	print "$lb butelek soku jabłkowego na ścianie, $lb butelek soku.";
+	print "$lb butelek soku jabłkowego na ścianie, $lb butelek soku\r\n";
 	$lb = $lb - 1;
-	print "Weż jedną i podaj dalej, $lb butelek soku na ścianie.";
+	print "Weż jedną i podaj dalej, $lb butelek soku na ścianie.\r\n";
 }
-print "$lb butelek soku jabłkowego na ścianie, $lb butelek soku.";
+print "$lb butelek soku jabłkowego na ścianie, $lb butelek soku.\r\n";
 $lb = $lb - 1;
-print "Weż jedną i podaj dalej, nie ma więcej butelek soku na ścianie.";
-print "Nie ma więcej butelek soku na ścianie, nie ma więcej butelek soku.";
+print "Weż jedną i podaj dalej, nie ma więcej butelek soku na ścianie.\r\n";
+print "Nie ma więcej butelek soku na ścianie, nie ma więcej butelek soku.\r\n";
 $lb = 99;
-print "Idź do sklepu i kup więcej, $lb butelek soku na scianie.";
+print "Idź do sklepu i kup więcej, $lb butelek soku na scianie.\r\n";
 ?>
