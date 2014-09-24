d1f58bd35a2b77eda0dc1c6a25255b4dd6ab5efe
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-09-24 19:46:18 +0200

    Create butelki_sok.php
    Po co to jest. bo ja nie wiem i tego nie uzupełniam.
    Nie karz mnie za to!!!!
    Plisss.

---
+++ butelki_sok.php
@@ -0,0 +1,16 @@
+<?php
+$lb = 99;
+// lb to liczba butelek
+
+while($lb >= 2){
+	print "$lb butelek soku jabłkowego na ścianie, $lb butelek soku.";
+	$lb = $lb - 1;
+	print "Weż jedną i podaj dalej, $lb butelek soku na ścianie.";
+}
+print "$lb butelek soku jabłkowego na ścianie, $lb butelek soku.";
+$lb = $lb - 1;
+print "Weż jedną i podaj dalej, nie ma więcej butelek soku na ścianie.";
+print "Nie ma więcej butelek soku na ścianie, nie ma więcej butelek soku.";
+$lb = 99;
+print "Idź do sklepu i kup więcej, $lb butelek soku na scianie.";
+?>
