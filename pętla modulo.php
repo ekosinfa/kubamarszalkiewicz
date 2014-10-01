3083805d7b4b65df7d4c95d3e3013315ee8cbba2
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-10-01 12:03:46 +0200

    Update pętla modulo.php

--- pętla modulo.php
+++ pętla modulo.php
@@ -4,18 +4,11 @@ while ($licz<=100){
 	if ($licz % 5 == 0){
 		print "pięć ";
 	}
-	else{
-	}
 	if ($licz % 3 == 0){
 		print "trzy";
 	}
-	else {
-	}
 	if (($licz % 3 != 0) and ($licz % 5 != 0)){
 		print $licz;
-	}
-	else{
-		
 	}
 	print "\n";
 		$licz = $licz +1;
