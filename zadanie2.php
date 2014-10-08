dd55e68ec482a6baaa799bd4c0b889476f324887
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2014-10-08 18:39:55 +0200

    Update zadanie2.php

--- zadanie2.php
+++ zadanie2.php
@@ -1,17 +1,40 @@
 <?php
 //Państwa, miasta.
-//http://ideone.com/K2QlYQ
-$rzeczy = array(
+//Stwórz program wypisujący wyrazu. Pamiętaj, że ma to działać po zmienieniu
+//litery jak i po dodaniu kolenjej np. E.
+
+$slowa = array(
 	"panstwa"=>array(
-		'I'=>'Irlandia',
-		'D'=>'Dania'),
+		'A'=>'Anglia',
+		'B'=>'Belgia',
+		'C'=>'Czechy',
+		'D'=>'Dania',
+		),
 	"miasta"=>array(
-		'I'=>'Istad',
-		'D'=>'Dom'),
+		'A'=>'Augustów',
+		'B'=>'Białystok',
+		'C'=>'Chorzów',
+		'D'=>'Darłówko'),
+	"rzecz"=>array(
+		'A'=>'Aparat',
+		'B'=>'Biurko',
+		'C'=>'Czapka',
+		'D'=>'Długopis'),
+	"roslina"=>array(
+		'A'=>'Arbuz',
+		'B'=>'Burak',
+		'C'=>'Chryzantema',
+		'D'=>'Dąb'),
+	"zwierzeta"=>array(
+		'A'=>'Antylopa',
+		'B'=>'Bażant',
+		'C'=>'Chomik',
+		'D'=>'Delfin'),
+	"imie"=>array(
+		'A'=>'Agnieszka',
+		'B'=>'Bartek',
+		'C'=>'Cecylia',
+		'D'=>'Dominik'),
 	);
 
-$litera = 'I;'
-
-foreach($rzeczy as $rzecz){
-	print $rzecz["$litera"];
-}
+?>
